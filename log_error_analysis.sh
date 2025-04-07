d61d5120515c725f974b179bc15fc66b461ea9f5
Oonnikannan Vijayakumar <unnikannanvj@gmail.com>
2025-04-07 12:47:53 -0700

    Create script log_error_analysis.sh to analyse error lines in logs

---
+++ log_error_analysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOG_FILE="/tmp/sample.log"
+
+if [ ! -f "$LOG_FILE" ]; then
+  echo "Log file not found!"
+  exit 1
+fi
+
+grep "ERROR" "$LOG_FILE" | while read -r line; do
+  echo -e "${line//ERROR/\e[31mERROR\e[0m}"
+done
